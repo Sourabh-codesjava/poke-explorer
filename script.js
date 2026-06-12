ddad4d748f18cf7567c8eaffab500615788b78f1
Sourabh <im.sourabh14@gmail.com>
2026-06-12 16:05:30 +0530

    Extract Pokemon data from API response

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const button = document.getElementById("search-btn");
 const pokemonInput = document.getElementById("pokemon-input");
 
-button.addEventListener("click", () => {
-    const pokemonName = pokemonInput.value;
-    
-    console.log(pokemonName);
+button.addEventListener("click", async () => {
+const pokemonName = pokemonInput.value.trim().toLowerCase();
+
+const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+);
+
+const data = await response.json();
+
+console.log("Name:", data.name);
+console.log("Height:", data.height);
+console.log("Weight:", data.weight);
+console.log("Image:", data.sprites.front_default);
+console.log("Type:", data.types[0].type.name);
 });
